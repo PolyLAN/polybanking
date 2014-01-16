b9443416e9e52ceffcd28da003f212d19ead9492
gcmalloc <gcmalloc@gmail.com>
2014-01-16 08:38:05 +0100

    updating url to the generic key method

--- server/configs/urls.py
+++ server/configs/urls.py
@@ -9,11 +9,9 @@ urlpatterns = patterns(
     url(r'^(?P<pk>[0-9]+)/show/$', 'show'),
     url(r'^(?P<pk>[0-9]+)/edit/$', 'edit'),
     url(r'^(?P<pk>[0-9]+)/logs/$', 'show_logs'),
-    
+
     #url(r'^(?P<pk>[0-9]+)/delete/$', 'delete'),
 
-    url(r'^(?P<pk>[0-9]+)/keys/ipn/new/$', 'new_ipn_key'),
-    url(r'^(?P<pk>[0-9]+)/keys/requests/new/$', 'new_requests_key'),
-    url(r'^(?P<pk>[0-9]+)/keys/api/new/$', 'new_api_key'),
+    url(r'^(?P<pk>[0-9]+)/keys/(?P<key_type>(ipn|requests|api))/new/$', 'new_ipn_key'),
 
 )
