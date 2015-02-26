cb6b0036b61a29b64930ad3e69ea22178f12be30
Maximilien Cuony <maximilien@theglu.org>
2015-02-26 17:43:38 +0100

    Typo users

--- server/users/urls.py
+++ server/users/urls.py
@@ -6,8 +6,8 @@ urlpatterns = patterns(
     'users.views',
 
     url(r'^$', 'list'),
-    url(r'^(?P<pk>[0-9]?)/show/$', 'show'),
-    url(r'^(?P<pk>[0-9]?)/edit/$', 'edit'),
-    url(r'^(?P<pk>[0-9]?)/delete/$', 'delete'),
+    url(r'^(?P<pk>[0-9]+)/show/$', 'show'),
+    url(r'^(?P<pk>[0-9]+)/edit/$', 'edit'),
+    url(r'^(?P<pk>[0-9]+)/delete/$', 'delete'),
 
 )
